279f9da051141fb9b6c7e559ba15fe80ef7d0d4c
Ramisettisanthoshi <143481554+Ramisettisanthoshi@users.noreply.github.com>
2023-10-02 21:13:51 +0530

    #include<stdio.h> int main() {     int average,boy1,boy2;     scanf("%d",&average);     scanf("%d",&boy1);     scanf("%d",&boy2);     int boy3=(average*3)-(boy1+boy2);     printf("%d",boy3);
    }

---
+++ Average_weight.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int average,boy1,boy2;
+    scanf("%d",&average);
+    scanf("%d",&boy1);
+    scanf("%d",&boy2);
+    int boy3=(average*3)-(boy1+boy2);
+    printf("%d",boy3);
+    
+}
