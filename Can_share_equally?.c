2a4ad65040ff239272fceba6c4dadf7241ef74c3
Ramisettisanthoshi <143481554+Ramisettisanthoshi@users.noreply.github.com>
2023-10-14 09:57:59 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d",&x);     scanf("%d",&y);     if (x == 0 && y % 2 == 0){             printf("YES");             }             else if (x == 0 && y % 2 != 0){                 printf("NO");             }             else if ((x + (2 * y)) % 2 == 0){                 printf("YES");             }             else {                 printf("NO");             } }

--- Can_share_equally?.c
+++ Can_share_equally?.c
@@ -1,28 +1,19 @@
-/* package codechef; // don't place package name! */
-
 #include<stdio.h>
-
-
-
-    int main()
-    {
-        
-            
-            int x;
-            int y;
-            scanf("%d%d",&x,&y);
-            if (x == 0 && y % 2 == 0){
-                printf("YES");
+int main()
+{
+    int x,y;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    if (x == 0 && y % 2 == 0){
+            printf("YES");
             }
             else if (x == 0 && y % 2 != 0){
                 printf("NO");
             }
             else if ((x + (2 * y)) % 2 == 0){
-            printf("YES");
+                printf("YES");
             }
             else {
                 printf("NO");
             }
-            
-        
-    }
+}
